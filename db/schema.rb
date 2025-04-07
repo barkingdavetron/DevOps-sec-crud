89d34c77ebae91cecd711a0c5766daba496925d6
david <mcgluedavid@gmail.com>
2025-04-07 16:09:43 +0100

    added deadline for tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_06_211810) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_07_150738) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,5 +18,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_06_211810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.datetime "deadline"
   end
 end
